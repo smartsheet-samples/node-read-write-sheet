9b6fe2d4d9f219d82db586e4f78fad716ff9d449
Steve Weil <steve.weil@smartsheet.com>
2017-04-04 15:47:57 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # node-read-write-sheet
 Node.js sample application that loads a sheet, updates selected cells, and saves the results
+
+This is a minimal Smartsheet sample that demonstrates how to
+* Load a sheet
+* Loop through the rows
+* Check for rows that meet a criteria
+* Update cell values
+* Write the results back to the original sheet
+
+
+This sample scans a sheet for rows where the value of the "Status" column is "Complete" and sets the "Remaining" column to zero.
+This is implemented in the `evaluate_row_and_build_updates()` method which you should modify to meet your needs.
+
+
+## Setup
+* Install the smartsheet library with `npm install smartsheet` at the command line
+* Import the sample data from "Sample Sheet.xlsx" into a new sheet
+
+* Update the `config.json` file with these two settings:
+    * An API access token, obtained from the Smartsheet Account button, under Personal settings
+    * The Sheet Id, obtained from sheet properties 
+
+* Run the application using your preferred IDE or at the command line with `node node-read-write-sheet.js` 
+
+The rows marked "Complete" will have the "Remaining" value set to 0. (Note that you will have to refresh in the desktop application to see the changes)
+
+## See also
+- http://smartsheet-platform.github.io/api-docs/
+- https://github.com/smartsheet-platform/smartsheet-javascript-sdk
+- https://www.smartsheet.com/
---
+++ node-read-write-sheet.js
@@ -0,0 +1,99 @@
+console.log('Starting');
+
+// TODO: Edit config.json to set desired sheet id and API token
+var config = require("./config.json");
+
+var token = config.SMARTSHEET_ACCESS_TOKEN;
+
+// If not specified in config file, use API token from environment variable "SMARTSHEET_ACCESS_TOKEN"
+if (!token)
+    token = process.env.SMARTSHEET_ACCESS_TOKEN
+
+var sheetId = config.sheetId;
+
+// If not found, install SDK package with command line: npm install smartsheet
+var client = require('smartsheet');
+
+// The API identifies columns by Id, but it's more convenient to refer to column names. Store a map here
+var columnMap = {};
+
+// Helper function to find cell in a row
+function getCellByColumnName(row, columnName) {
+    var columnId = columnMap[columnName];
+    return row.cells.find(function(c) {
+        return (c.columnId == columnId);
+    });
+}
+
+// TODO: Replace the body of this function with your code
+// This *example* looks for rows with a "Status" column marked "Complete" and sets the "Remaining" column to zero
+//
+// Return a new Row with updated cell values, else null to leave unchanged
+function evaluateRowAndBuildUpdates(sourceRow) {
+    var rowToUpdate = null;
+
+    // Find the cell and value to evaluate
+    var statusCell = getCellByColumnName(sourceRow, "Status");
+    if (statusCell.displayValue == "Complete") {
+        var remainingCell = getCellByColumnName(sourceRow, "Remaining");
+        if (remainingCell.displayValue != "0") { // Skip if already 0
+            console.log("Need to update row # " + sourceRow.rowNumber);
+
+            // Build updated row with new cell value
+            rowToUpdate = {
+                id: sourceRow.id,
+                cells: [{
+                    columnId: columnMap["Remaining"],
+                    value: 0
+                }]
+            };
+        }
+    }
+    return rowToUpdate;
+}
+
+// Initialize client SDK
+var ss = client.createClient({ accessToken: token });
+
+// Load entire sheet
+ss.sheets.getSheet({ id: sheetId })
+    .then(function(sheet) {
+        console.log("Loaded " + sheet.rows.length + " rows from sheet '" + sheet.name + "'");
+
+        // Build column map for later reference - converts column name to column id
+        sheet.columns.forEach(function(column) {
+            columnMap[column.title] = column.id;
+        });
+
+        // Accumulate rows needing update here
+        var rowsToUpdate = [];
+
+        // Evaluate each row in sheet
+        sheet.rows.forEach(function(row) {
+            var rowToUpdate = evaluateRowAndBuildUpdates(row);
+            if (rowToUpdate)
+                rowsToUpdate.push(rowToUpdate);
+        });
+
+        if (rowsToUpdate.length == 0) {
+            console.log("No updates required");
+        } else {
+            // Finally, write all updated cells back to Smartsheet
+            console.log("Writing " + rowsToUpdate.length + " rows back to sheet id " + sheet.id);
+
+            var updateRowArgs = {
+                body: rowsToUpdate,
+                sheetId: sheet.id
+            };
+
+            ss.sheets.updateRow(updateRowArgs)
+                .then(function(updatedRows) {
+                    console.log("Updated succeded");
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+
+        }
+        console.log("Done");
+    })
